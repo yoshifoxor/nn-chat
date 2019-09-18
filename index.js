37bb13a99eee8cd342007008aceb612d1f57b32c
yoshifoxor <kurmelsa5d@gmail.com>
2019-09-18 23:02:48 +0900

    認証された投稿の一覧表示機能

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 'use strict';
 const http = require('http');
+const auth = require('http-auth');
 const router = require('./lib/router');
 
-const server = http.createServer((req, res) => {
+const basic = auth.basic({
+  realm: 'Enter username and password.',
+  file: './users.htpasswd'
+});
+
+const server = http.createServer(basic, (req, res) => {
   router.route(req, res);
 }).on('error', (e) => {
   console.error('Server Error', e);
