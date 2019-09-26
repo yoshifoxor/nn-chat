063da2482f17df3e073da735cba8c04ceaeed80a
yoshifoxor <kurmelsa5d@gmail.com>
2019-09-27 06:35:50 +0900

    セッション固定化攻撃脆弱性の対策

--- test.js
+++ test.js
@@ -4,14 +4,16 @@ const assert = require('assert');
 
 // pug のテンプレートにおける XSS 脆弱性のテスト
 const html = pug.renderFile('./views/posts.pug', {
-  posts: [{
-    id: 1,
-    content: '<script>alert(\'test\');</script>',
-    postedBy: 'guest1',
-    trackingCookie: 1,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }],
+  posts: [
+    {
+      id: 1,
+      content: "<script>alert('test');</script>",
+      postedBy: 'guest1',
+      trackingCookie: '2639292283224063_ddcc625203464a9e10af58fc3eb92eed7df4b9b5',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ],
   user: 'guest1'
 });
 
